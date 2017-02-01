3634c6d43cf67b39d14ad72a160334f815163e0a
Jonatan Gall <jonatangd.souza@gmail.com>
2017-02-01 12:21:47 -0200

    main class move to main package

--- src/EasyOrganizer/main/Main.java
+++ src/EasyOrganizer/main/Main.java
@@ -3,7 +3,7 @@
 * To change this template file, choose Tools | Templates
 * and open the template in the editor.
 */
-package EasyOrganizer.model;
+package EasyOrganizer.main;
 
 import EasyOrganizer.UI.Menu;
 import EasyOrganizer.UI.Register;
